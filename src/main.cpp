dee65b97f64e8983150eed468f7016a1c91ef40d
Srinath10X <srinathcreates@gmail.com>
2024-11-16 00:59:17 +0530

    refactor: moved constants to `include/constants.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@
  |     License:  GPLV3 (see LICENSE file at the root of the repo)     |
  |     Github :  https://github.com/srinath10x/ttype                  |
  |     Author :  Srinath10X                                           |
- *-------------------------------------------------------------------*/
+ *--------------------------------------------------------------------*/
 
+#include "../include/constants.h"
 #include <csignal>
 #include <cstdlib>
 #include <ctime>
@@ -21,27 +22,6 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <vector>
-
-#define HIDE_CURSOR "\033[?25l"
-#define SHOW_CURSOR "\033[?25h"
-#define WIPE_SCREEN "\033[2J\033[H"
-#define RESET "\033[0m"
-#define BLUE "\033[34m"
-#define RED_UNDERLINE "\033[4;31m"
-#define WHITE_BACKGROUND "\033[47m"
-#define BLACK "\033[30m"
-
-const std::string VERSION = "v1.0.1";
-
-const std::vector<std::string> words = {
-    "the",   "of",    "to",    "and",     "a",      "in",    "is",   "it",
-    "you",   "that",  "he",    "was",     "for",    "on",    "are",  "with",
-    "as",    "I",     "they",  "be",      "at",     "one",   "have", "this",
-    "from",  "or",    "by",    "not",     "word",   "but",   "what", "some",
-    "we",    "can",   "occur", "support", "nature", "range", "path", "log",
-    "meant", "shell", "neck",  "program", "public", "look",  "name", "bee",
-};
 
 void disable_raw_mode() {
   struct termios term;
