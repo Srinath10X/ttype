d622beadd27a0eda8561f28c1785407eab2916c2
Srinath10X <srinathcreates@gmail.com>
2024-11-18 05:05:40 +0530

    refactor: moved words vector to constants.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,10 @@
 #include <csignal>
 #include <cstdio>
 #include <iostream>
-#include <string>
 #include <sys/ioctl.h>
-#include <vector>
 
 TerminalHandler terminal;
 Timer timer;
-
-const std::vector<std::string> words = {
-    "the",   "of",    "to",    "and",     "a",      "in",    "is",   "it",
-    "you",   "that",  "he",    "was",     "for",    "on",    "are",  "with",
-    "as",    "I",     "they",  "be",      "at",     "one",   "have", "this",
-    "from",  "or",    "by",    "not",     "word",   "but",   "what", "some",
-    "we",    "can",   "occur", "support", "nature", "range", "path", "log",
-    "meant", "shell", "neck",  "program", "public", "look",  "name", "bee",
-};
 
 struct winsize window;
 
