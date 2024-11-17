5d021fac85e4054bc9c403015daa9ed1f7246bf9
Srinath10X <srinathcreates@gmail.com>
2024-11-18 03:35:08 +0530

    feat: moved constants to include

---
+++ include/constants.hpp
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#define HIDE_CURSOR "\033[?25l"
+#define SHOW_CURSOR "\033[?25h"
+#define WIPE_SCREEN "\033[2J\033[H"
+#define RESET "\033[0m"
+#define BLUE "\033[34m"
+#define RED_UNDERLINE "\033[4;31m"
+#define WHITE_BACKGROUND "\033[47m"
+#define BLACK "\033[30m"
+
+#endif // !CONSTANTS_HPP
