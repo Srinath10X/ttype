6d9323a510f544cc644f5742eece9d4e6e543512
Srinath10X <srinathcreates@gmail.com>
2024-11-19 22:52:46 +0530

    feat: new getContext() method is added

--- include/ui_handler.hpp
+++ include/ui_handler.hpp
@@ -1,8 +1,12 @@
 #ifndef UI_HANDLER_HPP
 #define UI_HANDLER_HPP
 
+#include <sys/ioctl.h>
+
 class UiHandler {
 public:
+  struct winsize window;
+  void getContext();
   void wipeAndResetScreen();
 };
 
