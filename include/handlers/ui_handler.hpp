94eedae29e1d24ef4a7c33b4d6360a907f129ced
Srinath10X <srinathcreates@gmail.com>
2024-11-26 00:20:55 +0530

    feat: new padding modules are added to ui_handler

--- include/handlers/ui_handler.hpp
+++ include/handlers/ui_handler.hpp
@@ -9,6 +9,8 @@ public:
 
   void getContext();
   void wipeAndResetScreen();
+  void paddingTop(unsigned top_padding);
+  void paddingLeft(unsigned left_padding);
   void paddingTopWithOffset(unsigned offset);
   void paddingLeftWithOffset(unsigned offset);
   void alignContentWithOffset(unsigned offsetX, unsigned offsetY);
