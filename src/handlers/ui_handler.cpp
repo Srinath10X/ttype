49bc754aa29b967ce14f9cd8eb0ac09fafc39c40
Srinath10X <srinathcreates@gmail.com>
2024-12-06 20:51:38 +0530

    style: formatted with clang-format

--- src/handlers/ui_handler.cpp
+++ src/handlers/ui_handler.cpp
@@ -7,23 +7,14 @@ void UiHandler::wipeAndResetScreen() { std::cout << WIPE_SCREEN RESET; }
 
 void UiHandler::getContext() { ioctl(STDOUT_FILENO, TIOCGWINSZ, &window); }
 
-void UiHandler::paddingLeft(unsigned left_padding) {
-	std::cout << std::string(left_padding, ' ');
-}
+void UiHandler::paddingLeft(unsigned left_padding) { std::cout << std::string(left_padding, ' '); }
 
-void UiHandler::paddingTop(unsigned top_padding) {
-	std::cout << std::string(top_padding, '\n');
-}
+void UiHandler::paddingTop(unsigned top_padding) { std::cout << std::string(top_padding, '\n'); }
 
-void UiHandler::paddingTopWithOffset(unsigned offset) {
-	std::cout << std::string((window.ws_row - offset) / 2, '\n');
-}
+void UiHandler::paddingTopWithOffset(unsigned offset) { std::cout << std::string((window.ws_row - offset) / 2, '\n'); }
 
-void UiHandler::paddingLeftWithOffset(unsigned offset) {
-	std::cout << std::string((window.ws_col - offset) / 2, ' ');
-}
+void UiHandler::paddingLeftWithOffset(unsigned offset) { std::cout << std::string((window.ws_col - offset) / 2, ' '); }
 
 void UiHandler::alignContentWithOffset(unsigned offsetX, unsigned offsetY) {
-	std::cout << std::string((window.ws_row - offsetY) / 2, '\n')
-						<< std::string((window.ws_col - offsetX) / 2, ' ');
+	std::cout << std::string((window.ws_row - offsetY) / 2, '\n') << std::string((window.ws_col - offsetX) / 2, ' ');
 }
